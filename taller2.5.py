b8c32c1baba84602ab18ac1bac7e4aaaf16accb3
elgururincon <davidrincont@gmail.com>
2017-05-02 12:12:40 -0500

    Create taller2.5.py

---
+++ taller2.5.py
@@ -0,0 +1,27 @@
+import random
+
+array = []
+
+for i in range (10):
+  array.append(random.randint(0 , 1 ))
+  
+print array
+
+t= int(len(array)/2)
+t= t*2
+tm = (len(array) - i + 1 )
+
+for i in range (t,0,-2):
+
+  for j in range (t,tm):
+    
+    cout0=0
+    cout1=0
+    
+    for k in range () :
+      
+      if k == 0:
+        cout0 = cout0 + 1
+        
+      else: 
+        cout1 = cout1 +1
